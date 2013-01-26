c5a5be4f015bba0ffe9eb485b633794bc431e3ba
Yizheng Liao <liaoyizheng@gmail.com>
2013-01-26 15:05:39 -0800

    start the class

---
+++ @NPNetwork/NPNetwork.m
@@ -0,0 +1,12 @@
+classdef NPNetwork<handle
+    % NPNetwork is a Matlab class which constructs and implements the no
+    % propagation adaptive neural network
+    
+    properties
+    end
+    
+    methods
+    end
+    
+end
+
